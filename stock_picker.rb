9a0f4d9485086d6f0f7b363834c78423a01e7fad
Justin Townley <justin.d.townley@gmail.com>
2022-08-23 12:25:17 -0700

    Finish exercise

--- stock_picker.rb
+++ stock_picker.rb
@@ -3,17 +3,17 @@ require 'pry-byebug'
 def stock_picker(prices)
 	result = [0, 0]
 	max = 0
-	prices.each_index do |index|
-		if index == prices.count - 1 
-			break
-		end
-		prices.each.with_index(index + 1) do |inner_index|
-			if prices.inner_index - prices.index > max
-				max = prices.inner_index - prices.index
-				result[0] = prices.index
-				result[1] = prices.inner_index
+	prices.each_index do |i|
+		for j in i + 1..prices.count - 1 do
+			test = prices[j] - prices[i]
+			if test > max
+				max = test
+				result[0] = i
+				result[1] = j
+			end
 		end
 	end
+	return result
 end
 
 p stock_picker([17,3,6,9,15,8,6,1,10])
