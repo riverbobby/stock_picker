e5acdafb9f2ad79c9779ef0bcb297c6724018307
Justin Townley <justin.d.townley@gmail.com>
2022-10-13 09:00:03 -0700

    Run rubocop autocorrect

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,19 +1,19 @@
 require 'pry-byebug'
 
 def stock_picker(prices)
-	result = [0, 0]
-	max = 0
-	prices.each_index do |i|
-		for j in i + 1..prices.count - 1 do
-			test = prices[j] - prices[i]
-			if test > max
-				max = test
-				result[0] = i
-				result[1] = j
-			end
-		end
-	end
-	return result
+  result = [0, 0]
+  max = 0
+  prices.each_index do |i|
+    for j in i + 1..prices.count - 1 do
+      test = prices[j] - prices[i]
+      next unless test > max
+
+      max = test
+      result[0] = i
+      result[1] = j
+    end
+  end
+  result
 end
 
-p stock_picker([17,3,6,9,15,8,6,1,10])
+p stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
